3f58516b954366276a5b4495d3fe8e899bb58bbb
David Chen <dchen07@protonmail.com>
2022-07-16 13:20:42 -0400

    Add get_user shallow, refactor get_user_data to be get_repos
    Shallow user data for basic display/listing
    get_repos to get the repository data for final parsing/display

--- server/utils/github_api.js
+++ server/utils/github_api.js
@@ -4,64 +4,77 @@ const octokit = require("octokit");
 
 console.log(process.env.GH_TOKEN);
 const session = new octokit.Octokit({ auth: process.env.GH_TOKEN });
+// https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
+// greatly increased rate-limits if authentication is included
 
-async function get_user_data(username) {
-	console.log(username);
-	// let repos = (await session.request(`GET /users/${username}/repos`)).data;
-	// let repos = (await session.request(`GET /users/${username}/pinnedItems`)).data;
-
-	let repos = await session.graphql(
-		`query getPinned($user: String!) {
-  			user(login: $user) {
-    			pinnedItems(first: 6, types: REPOSITORY) {
-      				nodes {
-        				... on Repository {
-							name
-							url
-							forkCount
-							homepageUrl
-							languages(first: 1) {
-								edges {
-									node {
-										name
-									}
-								}
-							}
-							watchers {
-								totalCount
-							}
-							stargazerCount
-						}
-        			}
-      			}
-    		}
-  		}`,
+async function get_user_shallow(username) {
+	// @param username: String representing the user's GitHub username (potentially provided with OAuth2 if we have the time)
+	// returns a somewhat minimized Object representing the user with some relevant profile data
+	// TODO: handle request errors :)
+	const user = await session.request(`GET /users/${username}`).then(res =>
 		{
-			user: username,
+			return {
+				"username": res.data.login,
+				"name": res.data.name,
+				"pfp": res.data.avatar_url,
+				"url": res.data.html_url,
+				"bio": res.data.bio,
+				"followers": res.data.followers,
+				"public_repos": res.data.public_repos,
+				"repos": []
+			}
 		}
 	);
 
-	repos = JSON.parse(JSON.stringify(repos));
-	repos = repos.user.pinnedItems.nodes;
-	if (repos.length < 1) throw "woops no repos";
+	// get all repos using a while loop (to respect GitHub pagination limits)
+	let repos_left = user.public_repos;
+	while (repos_left > 0) {
+		const repoPage = (await session.request(`GET /users/${username}/repos?per_page=${Math.min(repos_left, 100)}`)).data.map(
+				(repo) => {
+					return {
+						"name": repo.full_name,
+						"url": repo.html_url,
+						"stars": repo.stargazers_count
+					}
+				}
+			);
+		user.repos = [...user.repos, ...repoPage];
+		repos_left -= 100;
+	}
 
-	// only return non-fork public repositories that aren't templates
-	repos = repos.filter((repo) => {
-		return !(repo.fork || repo.private || repo.is_template);
-	});
-
-	return repos.map((repo) => {
-		return {
-			name: repo.name, // repository name
-			url: repo.homepageUrl || repo.url, // return linked site on sidebar, if unavailable, return GitHub repository
-			language: repo.languages.edges[0].node.name, // primary language
-
-			// repository statistic counts, may be worth sorting by
-			stars: repo.stargazerCount,
-			watchers: repo.watchers.totalCount,
-			forks_count: repo.forkCount,
-		};
-	});
+	return user;
 }
 
-module.exports = { get_user_data };
+async function get_repos(repositories) {
+	// @param repository - Array of Objects {owner: string, name: string}
+
+	const repos = [];
+
+	for (const repo in repositories) {
+		console.log(repositories[repo]);
+		repos.push(await session.graphql(
+			`query getRepo($owner: String!, $name: String!) {
+				repository(owner: $owner, name: $name) {
+					name
+					url
+					languages(first: 3) {
+						edges {
+							node {
+								name
+							}
+						}
+					}
+					description
+				}
+			}`,
+			{owner: repositories[repo].owner, name: repositories[repo].name}
+			).then(res => res.repository)
+		);
+		repos[repo].languages = repos[repo].languages.edges.map(node => node.node.name);
+	}
+
+
+	return repos;
+}
+
+module.exports = { get_repos, get_user_shallow };
