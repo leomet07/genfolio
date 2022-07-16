924e3e6ee1cbb7c139d970351ad8654ec619d297
leomet07 <leomet07@gmail.com>
2022-07-16 01:18:04 -0400

    code cleanup

--- server/utils/github_api.js
+++ server/utils/github_api.js
@@ -3,16 +3,15 @@ dotenv.config();
 const octokit = require("octokit");
 
 console.log(process.env.GH_TOKEN);
-const session = new octokit.Octokit({auth: process.env.GH_TOKEN})
-
+const session = new octokit.Octokit({ auth: process.env.GH_TOKEN });
 
 async function get_user_data(username) {
-    console.log(username);
-    // let repos = (await session.request(`GET /users/${username}/repos`)).data;
-    // let repos = (await session.request(`GET /users/${username}/pinnedItems`)).data;
+	console.log(username);
+	// let repos = (await session.request(`GET /users/${username}/repos`)).data;
+	// let repos = (await session.request(`GET /users/${username}/pinnedItems`)).data;
 
-    let repos = await session.graphql(
-        `query getPinned($user: String!) {
+	let repos = await session.graphql(
+		`query getPinned($user: String!) {
   			user(login: $user) {
     			pinnedItems(first: 6, types: REPOSITORY) {
       				nodes {
@@ -37,36 +36,32 @@ async function get_user_data(username) {
       			}
     		}
   		}`,
-        {
-            user: username
-        }
-    );
+		{
+			user: username,
+		}
+	);
 
 	repos = JSON.parse(JSON.stringify(repos));
 	repos = repos.user.pinnedItems.nodes;
 	if (repos.length < 1) throw "woops no repos";
-    console.log('repos:', repos);
-    // console.log('repos lang:', repos[0].languages.edges[0].node.name);
 
+	// only return non-fork public repositories that aren't templates
+	repos = repos.filter((repo) => {
+		return !(repo.fork || repo.private || repo.is_template);
+	});
 
-    // only return non-fork public repositories that aren't templates
-    repos = repos.filter((repo) => {
-        return (!(repo.fork || repo.private || repo.is_template))
-    });
+	return repos.map((repo) => {
+		return {
+			name: repo.name, // repository name
+			url: repo.homepageUrl || repo.url, // return linked site on sidebar, if unavailable, return GitHub repository
+			language: repo.languages.edges[0].node.name, // primary language
 
-    return repos.map((repo) => {
-        console.log(Object.keys(repo));
-        return {
-            "name": repo.name,   // repository name
-            "url": repo.homepageUrl || repo.url,  // return linked site on sidebar, if unavailable, return GitHub repository
-            "language": repo.languages.edges[0].node.name,  // primary language
-
-            // repository statistic counts, may be worth sorting by
-            "stars": repo.stargazerCount,
-            "watchers": repo.watchers.totalCount,
-            "forks_count": repo.forkCount
-        }
-    });
+			// repository statistic counts, may be worth sorting by
+			stars: repo.stargazerCount,
+			watchers: repo.watchers.totalCount,
+			forks_count: repo.forkCount,
+		};
+	});
 }
 
-module.exports = {get_user_data};
+module.exports = { get_user_data };
--- server/utils/test.js
+++ server/utils/test.js
@@ -1,7 +1,5 @@
-const {get_user_data} = require("./github_api");
+const { get_user_data } = require("./github_api");
 
-
-(async function() {
-    console.log(await get_user_data("leomet07"));
-    console.log(await get_user_data("TheEgghead27"));
-}());
+(async function () {
+	console.log(await get_user_data("leomet07"));
+})();
