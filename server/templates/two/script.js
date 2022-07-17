c9a2e28c80b85a9981e71bdcf9a8f4a43526e164
leomet07 <leomet07@gmail.com>
2022-07-16 21:10:26 -0400

    Added template two

---
+++ server/templates/two/script.js
@@ -0,0 +1,22 @@
+window.onload = async function () {
+	let app = document.getElementById("typewriter_div");
+
+	let typewriter = new Typewriter(app, {
+		loop: true,
+		skipAddStyles : true
+	});
+
+	const span_elements = document.getElementsByClassName("hidden_span");
+	const spans = [];
+	for (const span of span_elements) {
+		spans.push(span.innerHTML);
+	}
+
+	for(let i = 0; i < spans.length; i++) {
+		typewriter.typeString(spans[i]);
+		typewriter.pauseFor(1000);
+		typewriter.deleteAll();
+	}
+	typewriter.start();
+	
+};
