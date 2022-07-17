fa61b365bb3b630378680a99de8750c741479738
leomet07 <leomet07@gmail.com>
2022-07-16 20:01:13 -0400

    repo searching

--- client/src/routes/editor.svelte
+++ client/src/routes/editor.svelte
@@ -7,6 +7,8 @@
 	let page = "page1";
 	let repos = [];
 	let repoSearch = "";
+	let reposSelected = [];
+	let sortedRepos = [];
 	import { github_username } from "../store";
 	import About from "./About.svelte";
 	// import A1 from "./templates/1.svelte";
@@ -14,10 +16,17 @@
 	import { onMount } from "svelte";
 	$github_username = "Test";
 
+	const setSortedRepos = () => {
+		console.log("sorting", repoSearch, repos);
+		sortedRepos = repos.filter(element => {
+			return element.name.toLowerCase().includes(repoSearch.toLowerCase());
+		})
+	}
+
 	onMount(async () => {
 		const user_data = await fetch_user_data("leomet07");
 		repos = user_data.repos;
-		console.log(repos);
+		sortedRepos = repos;
 	});
 </script>
 
@@ -64,7 +73,6 @@
 						class="add"
 						on:click={() => {
 							if (currentString.length > 0) {
-								// strings.push(currentString);
 								strings = [...strings, currentString];
 								currentString = "";
 								console.log(strings);
@@ -93,24 +101,42 @@
 			<h1 class="header">Select Projects</h1>
 			<div class="divider" />
 			<div class="info">
+				<h2 class="repo-select-info"><spans style="color: #c25eff">{reposSelected.length}</spans> of 6 repos selected</h2>
 				<input
 					on:keyup={(e) => {
 						repoSearch = e.target.value;
+						setSortedRepos();
 					}}
 					class="input"
 					type="text"
 					placeholder="Search for your repo"
 				/>
-				<div class="repo-grid">
-					{#each repos as repo, index}
-						{#if index < 10 && repo.name.toLowerCase().includes(repoSearch)}
-						<div class="grid-item">
-							<h3>{repo.name}</h3>
+				<div class="repo-grid"> 
+					{#each sortedRepos as repo, index}
+						{#if index < 8}
+						<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+						<div class="grid-item" 
+							on:click={() => {
+								if(reposSelected.includes(index)) {
+									let id = reposSelected.indexOf(index);
+									reposSelected.splice(id, 1);
+									reposSelected = reposSelected;
+								}
+								else {
+									reposSelected = [...reposSelected, index];
+								}
+							}}
+						>
+							<h3 class="repo-title">{repo.name}</h3>
+							{#if reposSelected.includes(index)}
+								<h4 style="color: #c25eff">selected</h4>
+							{/if}
 						</div>
 						{/if}
 					{/each}
 				</div>
 			</div>
+			<button class="next-button" on:click={() => {page = "page2"}}>Next</button>
 		{/if}
 
 
@@ -243,19 +269,37 @@
 	}
 	.repo-grid {
 		width: 100%;
+		margin-top: 2rem;
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		grid-gap: 2rem;
 	}
 	.grid-item {
 		height: 10vh;
-		background-color: grey;
+		background-color: rgb(18, 19, 30);
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		border-radius: 1rem;
+		transition: 0.25s;
+		flex-direction: column;
 	}
-	.search {
-		border: none;
+	.grid-item:hover {
+		cursor: pointer;
+		transform: scale(1.05);
+	}
+	.repo-title {
+		color: white;
+		font-size: 1rem;
+		font-family: "IBM Plex Mono", sans-serif;
+		font-weight: 400;
+	}
+	.repo-select-info {
+		color: white;
+		font-size: 1rem;
+		font-family: "IBM Plex Mono", sans-serif;
+		font-weight: 400;
+		margin-top: 1rem;
 	}
 	textarea {
 		resize:none;
