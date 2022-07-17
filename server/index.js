415097d9d70aeebc22da76b8786d40fbbfbcaf77
David Chen <dchen07@protonmail.com>
2022-07-17 00:21:27 -0400

    Add ratelimits

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 // const mongoose = require("mongoose");
 const morgan = require("morgan");
 const path = require("path");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const app = express();
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 // Only redirect to SSL if developer allows and states that machine running this has SSL to prevent crashes on computers without SSL
-if (process.env.SSL == "true") {
+if (process.env.SSL === "true") {
 	app.enable("trust proxy");
 
 	app.use(function (req, res, next) {
@@ -26,6 +27,16 @@ if (process.env.SSL == "true") {
 		res.redirect("https://" + req.headers.host + req.url);
 	});
 }
+
+// site-wide rate limit: 60 requests / 60 seconds
+const baseLimit = rateLimit({
+	max: 60,
+	windowMs: 60 * 1000,
+
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(baseLimit);
 
 // import Routes
 const apiRouter = require("./routes/api").router;
@@ -62,7 +73,7 @@ app.use(
 	})
 );
 
-//Routes Middleware
+// Routes Middleware
 app.use("/api/", apiRouter);
 
 app.use(
